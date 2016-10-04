b21e2352cf58206bf373aedd8e5fe6e9946ab780
dthursto <dthursto@ipa.nerdpail.com>
2016-10-04 08:21:41 -0400

    added some steps

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-if ! [ -e /tmp/csd-repos ]
+
+touch /tmp/csd-repos
+touch /tmp/csd-centos-mirror
+
+if ! [ -e /tmp/csd-repos ];
 then
   #subscription-manager register --auto-attach --username=user --password=passwd
   subscription-manager repos --enable rhel-server-rhscl-7-rpms
@@ -7,7 +11,7 @@ then
   touch /tmp/csd-repos
 fi
 
-if ! [ -e /tmp/csd-centos-mirror ]
+if ! [ -e /tmp/csd-centos-mirror ];
 then
   yum-config-manager --add-repo=http://mirror.centos.org/centos-7/7/sclo/x86_64/sclo/
   echo "gpgcheck=0" >> \
@@ -15,34 +19,40 @@ then
   touch /tmp/csd-centos-mirror
 fi
 
-yum groupinstall "Virtualization Host"
-systemctl start libvirtd
-systemctl enable libvirtd
+#yum groupinstall "Virtualization Host"
+#systemctl start libvirtd
+#systemctl enable libvirtd
 
-yum install sclo-vagrant1 sclo-vagrant1-vagrant-libvirt \
-  sclo-vagrant1-vagrant-libvirt-doc
+#yum install sclo-vagrant1 sclo-vagrant1-vagrant-libvirt \
+#  sclo-vagrant1-vagrant-libvirt-doc
 
-cp /opt/rh/sclo-vagrant1/root/usr/share/vagrant/gems/doc/vagrant-libvirt-*/polkit/10-vagrant-libvirt.rules \
-  /etc/polkit-1/rules.d
+#cp /opt/rh/sclo-vagrant1/root/usr/share/vagrant/gems/doc/vagrant-libvirt-*/polkit/10-vagrant-libvirt.rules \
+#  /etc/polkit-1/rules.d
 
-systemctl restart libvirtd
-systemctl restart polkit
+#systemctl restart libvirtd
+#systemctl restart polkit
 
-
-echo '''
-In another shell, perform the following steps to add your user to the vagrant group:
-$ echo $USER
-$ su -
-Password:
+#vagrantgroup=`id | grep vagrant`
+#echo $vagrantgroup
+#if [ -z $vagrantgroup ];
+#echo '''
+#In another shell, perform the following steps to add your user to the vagrant group:
+#$ echo $USER
+#$ su -
+#Password:
 # usermod -a -G vagrant <username>
-'''
-read
+#'''
 
+#if [ -z $vagrantgroup ];
+#read keypress
+
+cd ~
+echo `pwd`
 scl enable sclo-vagrant1 bash
 
-echo 'Download the "Red Hat Container Tools" and "RHEL 7.2 Vagrant box for libvirt"'
-echo 'from the following link: https://access.redhat.com/downloads/content/293/'
-read
+#echo 'Download the "Red Hat Container Tools" and "RHEL 7.2 Vagrant box for libvirt"'
+#echo 'from the following link: https://access.redhat.com/downloads/content/293/'
+#read keypress
 
 #cd
 #unzip ~/Downloads/cdk-2.2.0.zip
@@ -53,12 +63,11 @@ read
 #  ./vagrant-service-manager-*.gem \
 #  ./vagrant-sshfs-*.gem
 
-echo "The following plugins are installed"
+echo "The following plugins are installed";
 vagrant plugin list
 
 #vagrant box add --name cdkv2 \
 #  ~/Downloads/rhel-cdk-kubernetes-7.2-*.x86_64.vagrant-libvirt.box
-
+echo "Starting CDK....";
 cd ~/cdk/components/rhel/misc/shared_folder/rhel-ose/
-vagrant up
-
+vagrant up;
